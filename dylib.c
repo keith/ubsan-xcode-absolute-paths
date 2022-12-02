d3524f847246d498498a4cb808d956ddb733e3ee
Keith Smiley <keithbsmiley@gmail.com>
2022-12-01 16:22:34 -0800

    Cleanup dylib code

--- dylib.c
+++ dylib.c
@@ -1,91 +1,74 @@
-#include <stdio.h>
 #include <dlfcn.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "fishhook.h"
 
 void (*orig___ubsan_on_report)(void);
 
 void __ubsan_on_report(void) {
-    printf("my report 2\n");
+  // This report function does nothing but internally lldb sets a breakpoint on
+  // it and when that breakpoint fires it collects the info about the ubsan
+  // violation. It _has_ to retain the original name because otherwise lldb will
+  // choose the real ubsan library for the breakpoint, and the module associated
+  // with the breakpoint is how it decides where to call the reporter function
+  // from.
+
+  // Order of operations:
+  // 1. Library is loaded
+  // 2. lldb sets a breakpoint on the first instance of __ubsan_on_report it
+  //    finds (has to be this one)
+  // 3. We rebind the original __ubsan_on_report to this function
+  // 4. When ubsan calls the original function, it's redirected here, then the
+  //    breakpoint is hit in this module instead of the real ubsan library
 }
 
+typedef void (*reporter_t)(const char **OutIssueKind, const char **OutMessage,
+                           const char **OutFilename, unsigned *OutLine,
+                           unsigned *OutCol, char **OutMemoryAddr);
 
-void my__ubsan_on_report(void) {
-    printf("my report\n");
-//    orig___ubsan_on_report();
-    __ubsan_on_report();
-}
-
-typedef void (*foo_t)(const char **OutIssueKind,
-    const char **OutMessage, const char **OutFilename, unsigned *OutLine,
-                                unsigned *OutCol, char **OutMemoryAddr);
-
-
-__attribute__((visibility("default")))
-void
-__ubsan_get_current_report_data(const char **OutIssueKind,
-    const char **OutMessage, const char **OutFilename, unsigned *OutLine,
-                                unsigned *OutCol, char **OutMemoryAddr)
-{
-
-//    const char *foo = malloc(10);
-//    foo = "lol";
-//    OutMessage = &foo;
-    printf("mine!\n");
-
-    foo_t realFoo = (foo_t)dlsym(RTLD_NEXT, "__ubsan_get_current_report_data");
-    printf("mine! %p\n", realFoo);
-
-    realFoo(OutIssueKind, OutMessage, OutFilename, OutLine, OutCol, OutMemoryAddr);
-
-    printf("the out filename is '%s'\n", *OutFilename);
-    *OutFilename = "/Users/ksmiley/Downloads/ub-paths/ub-paths/main.c";
-    printf("the out filename is now '%s'\n", *OutFilename);
-
-//    abort();
-//    fprintf(stderr, "mine firstttttttttt\n");
-//
-//    orig__ubsan_get_current_report_data(OutIssueKind, OutMessage, OutFilename, OutLine, OutCol, OutMemoryAddr);
-//
-//    FILE *fp = fopen("/tmp/reporter.txt", "w");
-//    if (fp == NULL)
-//    {
-////        printf("Error opening the file %s", filename);
-////        return -1;
-//        return;
-//    }
-//    // write to the text file
-//    for (int i = 0; i < 10; i++)
-//        fprintf(fp, "reporter 2 is the line #%d\n", i + 1);
-//
-//    // close the file
-//    fclose(fp);
-
-}
-
-__attribute__((constructor))
-  void foo() {
-
-        rebind_symbols((struct rebinding[2]){
-        {"__ubsan_on_report", (void *)my__ubsan_on_report, (void * *)&orig___ubsan_on_report},
-//        {"__ubsan_get_current_report_data", (void *)my___ubsan_get_current_report_data, (void * *)&orig___ubsan_get_current_report_data}
-    }, 1);
-
-    printf("loading\n");
+// This function has to match the name and signature from the real ubsan
+// library. It is called in response to a lldb breakpoint on __ubsan_on_report
+// and is chosen since it's in the same module as that function (see above).
+void __ubsan_get_current_report_data(const char **OutIssueKind,
+                                     const char **OutMessage,
+                                     const char **OutFilename,
+                                     unsigned *OutLine, unsigned *OutCol,
+                                     char **OutMemoryAddr) {
+  reporter_t real_reporter =
+      (reporter_t)dlsym(RTLD_NEXT, "__ubsan_get_current_report_data");
+  if (real_reporter == NULL) {
+    fprintf(stderr, "dlsym failed, this likely means the ubsan hacks library "
+                    "doesn't depend on ubsan, it needs to link against it\n");
+    abort();
   }
 
-//__attribute__((visibility("default")))
-//void __ubsan_on_report(void) {
-//    printf("my report\n");
-//}
+  real_reporter(OutIssueKind, OutMessage, OutFilename, OutLine, OutCol,
+                OutMemoryAddr);
+  if (strlen(*OutFilename) < 1 || strncmp("/", *OutFilename, 1) == 0) {
+    return;
+  }
 
+  const char *srcroot = getenv("SRCROOT");
+  if (srcroot == NULL) {
+    fprintf(stderr, "$SRCROOT not set for ubsan hacks, launch the tests with "
+                    "it in the environment to fix paths\n");
+    return;
+  }
 
-// __attribute__((visibility("default")))
-// void
-// __ubsan_get_current_report_data(const char **OutIssueKind,
-//     const char **OutMessage, const char **OutFilename, unsigned *OutLine,
-//                                 unsigned *OutCol, char **OutMemoryAddr)
-// {
-//     printf("mine firstttttttttt\n");
-// }
+  char *new_filename = malloc(1024);
+  strcpy(new_filename, srcroot);
+  strcat(new_filename, "/");
+  strcat(new_filename, *OutFilename);
+  *OutFilename = new_filename;
+}
 
+__attribute__((constructor)) void setup_hacks() {
+  rebind_symbols(
+      (struct rebinding[2]){
+          {"__ubsan_on_report", (void *)__ubsan_on_report,
+           (void **)&orig___ubsan_on_report},
+      },
+      1);
+}
