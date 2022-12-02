cca5f7f02778bb1fb2178e219a2453296ce3ca0c
Keith Smiley <keithbsmiley@gmail.com>
2022-12-01 16:31:25 -0800

    Add note about why compiler.py exists

--- ub-paths/compiler.py
+++ ub-paths/compiler.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# NOTE: This just exists to simplify getting relative paths in the binary. If
+# you're using a build system that passes file paths relatively you already get
+# this (and if you don't you shouldn't need this hack at all)
+
 import subprocess
 import sys
 import os
